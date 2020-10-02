8f649e9373e57a21813042d8d44901fb9ff1fe1c
Henry Peterson <hankpete97@gmail.com>
2020-10-01 19:19:58 -0700

    working on modified 1D equations

--- fdm/inversion.jl
+++ fdm/inversion.jl
@@ -288,3 +288,172 @@ function getChiEkman(b)
 
     return chiEkman
 end
+
+"""
+    LHS = getInversionLHS1DAdjusted()
+
+Setup left hand side of linear system for adjusted 1D problem.
+"""
+function getInversionLHS1DAdjusted()
+    nPts = nξ*nσ
+
+    umap = reshape(1:nPts, nξ, nσ)    
+    A = Tuple{Int64,Int64,Float64}[]  
+
+    # for finite difference on the top and bottom boundary
+    fd_bot = mkfdstencil(σ[1:3], σ[1], 1)
+    fd_top = mkfdstencil(σ[nσ-2:nσ], σ[nσ], 1)
+    fd_top_σσ = mkfdstencil(σ[nσ-3:nσ], σ[nσ], 2)
+
+    # Main loop, insert stencil in matrix for each node point
+    for i=1:nξ
+        # Lower boundary conditions 
+        # b.c. 1: dσ(chi) = 0
+        push!(A, (umap[i, 1], umap[i, 1], fd_bot[1]))
+        push!(A, (umap[i, 1], umap[i, 2], fd_bot[2]))
+        push!(A, (umap[i, 1], umap[i, 3], fd_bot[3]))
+        # b.c. 2: chi = 0 
+        push!(A, (umap[i, 2], umap[i, 1], 1.0))
+
+        # Upper boundary conditions
+        # b.c. 1: dσ(chi) = 0
+        push!(A, (umap[i, nσ], umap[i, nσ-2], fd_top[1]))
+        push!(A, (umap[i, nσ], umap[i, nσ-1], fd_top[2]))
+        push!(A, (umap[i, nσ], umap[i, nσ],   fd_top[3]))
+        #= # b.c. 1: dσσ(chi) = 0 (or -stress at top) =#
+        #= push!(A, (umap[i, nσ], umap[i, nσ-3], fd_top_σσ[1])) =#
+        #= push!(A, (umap[i, nσ], umap[i, nσ-2], fd_top_σσ[2])) =#
+        #= push!(A, (umap[i, nσ], umap[i, nσ-1], fd_top_σσ[3])) =#
+        #= push!(A, (umap[i, nσ], umap[i, nσ],   fd_top_σσ[4])) =#
+        # b.c. 2: chi = U
+        push!(A, (umap[i, nσ-1], umap[i, nσ], 1.0))
+
+        # Interior nodes
+        for j=3:nσ-2
+            row = umap[i, j] 
+
+            # dσ stencil
+            fd_σ = mkfdstencil(σ[j-1:j+1], σ[j], 1)
+            κ_σ = sum(fd_σ.*κ[i, j-1:j+1])
+
+            # dσσ stencil
+            fd_σσ = mkfdstencil(σ[j-1:j+1], σ[j], 2)
+            κ_σσ = sum(fd_σσ.*κ[i, j-1:j+1])
+
+            # dσσσ stencil
+            fd_σσσ = mkfdstencil(σ[j-2:j+2], σ[j], 3)
+
+            # dσσσσ stencil
+            fd_σσσσ = mkfdstencil(σ[j-2:j+2], σ[j], 4)
+            
+            # eqtn: dσσ(nu*dσσ(chi))/H^4 + f^2*cos(θ)^2(chi - U)/nu = -dσ(b)*sin(θ)/H
+            # term 1 (product rule)
+            push!(A, (row, umap[i, j-1], Pr*κ_σσ*fd_σσ[1]/H(ξ[i])^4))
+            push!(A, (row, umap[i, j],   Pr*κ_σσ*fd_σσ[2]/H(ξ[i])^4))
+            push!(A, (row, umap[i, j+1], Pr*κ_σσ*fd_σσ[3]/H(ξ[i])^4))
+
+            push!(A, (row, umap[i, j-2], 2*Pr*κ_σ*fd_σσσ[1]/H(ξ[i])^4))
+            push!(A, (row, umap[i, j-1], 2*Pr*κ_σ*fd_σσσ[2]/H(ξ[i])^4))
+            push!(A, (row, umap[i, j],   2*Pr*κ_σ*fd_σσσ[3]/H(ξ[i])^4))
+            push!(A, (row, umap[i, j+1], 2*Pr*κ_σ*fd_σσσ[4]/H(ξ[i])^4))
+            push!(A, (row, umap[i, j+2], 2*Pr*κ_σ*fd_σσσ[5]/H(ξ[i])^4))
+
+            push!(A, (row, umap[i, j-2], Pr*κ[i, j]*fd_σσσσ[1]/H(ξ[i])^4))
+            push!(A, (row, umap[i, j-1], Pr*κ[i, j]*fd_σσσσ[2]/H(ξ[i])^4))
+            push!(A, (row, umap[i, j],   Pr*κ[i, j]*fd_σσσσ[3]/H(ξ[i])^4))
+            push!(A, (row, umap[i, j+1], Pr*κ[i, j]*fd_σσσσ[4]/H(ξ[i])^4))
+            push!(A, (row, umap[i, j+2], Pr*κ[i, j]*fd_σσσσ[5]/H(ξ[i])^4))
+            # term 2 (cos(θ)^2 = 1/(1 + Hx^2))
+            push!(A, (row, umap[i, j], f^2/(1 + Hx(ξ[i])^2)/(Pr*κ[i, j])))
+        end
+    end
+
+    # Create CSC sparse matrix from matrix elements
+    A = sparse((x->x[1]).(A), (x->x[2]).(A), (x->x[3]).(A), nPts, nPts)
+
+    return A
+end
+
+"""
+    RHS = getInversionRHS1DAdjusted(b)
+
+Setup right hand side of linear system for the adjusted 1D problem.
+"""
+function getInversionRHS1DAdjusted(b)
+    # sin(θ)
+    sinθ = @. -Hx(ξξ)/sqrt(1 + Hx(ξξ)^2)
+    # cos(θ)
+    cosθ = @. 1/sqrt(1 + Hx(ξξ)^2) 
+
+    #= U = 0 # for now set vertically integrated cross-slope flow to zero =#
+    U = @. κ0*cosθ/sinθ
+    #= ridgePlot(U, b, "U", "") =#
+
+    nPts = nξ*nσ
+    umap = reshape(1:nPts, nξ, nσ)    
+
+    # -dẑ(b)*sin(θ) = -(-sin(θ)*dx(b) + cos(θ)*dz(b))*sin(θ)
+    rhs = -(-sinθ.*xDerivativeTF(b) .+ cosθ.*zDerivativeTF(b)).*sinθ
+
+    # reshape
+    rhs = reshape(rhs, nPts, 1)
+
+    # chi = U at top
+    rhs[umap[:, nσ - 1]] = U[:, 1]
+
+    return rhs
+end
+
+"""
+    chi, u, v, w = postProcess1DAdjusted(sol)
+
+Take solution `sol` for adjusted 1D problem and extract reshaped `chi`. Compute `u`, `v`, `w`
+from definition of chi and equations.
+"""
+function postProcess1DAdjusted(sol)
+    # sin(θ)
+    sinθ = @. -Hx(ξξ)/sqrt(1 + Hx(ξξ)^2)
+    # cos(θ)
+    cosθ = @. 1/sqrt(1 + Hx(ξξ)^2) 
+
+    #= U = 0 # for now set vertically integrated cross-slope flow to zero =#
+    U = @. κ0*cosθ/sinθ
+
+    # reshape 
+    chi = reshape(sol[1:end], nξ, nσ)
+
+
+    # compute û = dẑ(chi)
+    û = -sinθ.*xDerivativeTF(chi) .+ cosθ.*zDerivativeTF(chi)
+
+    # compute u
+    u = cosθ.*û
+
+    # compute w
+    w = sinθ.*û
+
+    # compute v = int_-1^0 f*chi/nu dσ*H
+    v = zeros(nξ, nσ)
+    for i=1:nξ
+        v[i, :] = cumtrapz(f*cosθ[i, :].*(chi[i, :] .- U[i, :])./(Pr*κ[i, :]), σ)*H(ξ[i])
+    end
+
+    return chi, u, v, w
+end
+
+
+#= chi, uξ, uη, uσ, U = invert(b, inversionLHS) =#
+#= u, v, w = transformFromTF(uξ, uη, uσ) =#
+#= ridgePlot(chi, b, "chi", "") =#
+#= ridgePlot(u, b, "u", "") =#
+ridgePlot(v, b, "v", "")
+#= ridgePlot(w, b, "w", "") =#
+
+#= inversionLHS1DAdjusted = getInversionLHS1DAdjusted() =#
+inversionRHS1DAdjusted = getInversionRHS1DAdjusted(b)
+sol = inversionLHS1DAdjusted\inversionRHS1DAdjusted
+chi1D, u1D, v1D, w1D = postProcess1DAdjusted(sol)
+#= ridgePlot(chi1D, b, "chi 1D", ""; vext=maximum(abs.(chi))) =#
+#= ridgePlot(u1D, b, "u 1D", ""; vext=maximum(abs.(u))) =#
+ridgePlot(v1D, b, "v 1D", ""; vext=maximum(abs.(v)))
+#= ridgePlot(w1D, b, "w 1D", "") =#
